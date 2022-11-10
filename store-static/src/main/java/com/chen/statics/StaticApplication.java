b0a381d85a69f6bfcf2d2abd572d22af0cfb0d8c
cs <1006596474@qq.com>
2022-11-10 18:13:22 +0800

    完成静态资源服务

---
+++ store-static/src/main/java/com/chen/statics/StaticApplication.java
@@ -0,0 +1,17 @@
+package com.chen.statics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 静态资源应用程序
+ *
+ * @author 10065
+ * @date 2022/11/10
+ */
+@SpringBootApplication
+public class StaticApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StaticApplication.class,args);
+    }
+}
